a6f7da8ef0e803bd86b8919932358bbec5997a38
sunweiye.3 <sunweiye.3@bytedance.com>
2020-08-12 23:48:48 +0800

    test

---
+++ src/main/java/com/sunweiye/flink/java/day02/_7ReduceTransformation.java
@@ -0,0 +1,51 @@
+package com.sunweiye.flink.java.day02;
+
+import org.apache.flink.api.common.functions.FlatMapFunction;
+import org.apache.flink.api.common.functions.ReduceFunction;
+import org.apache.flink.api.java.tuple.Tuple;
+import org.apache.flink.api.java.tuple.Tuple2;
+import org.apache.flink.streaming.api.datastream.DataStreamSource;
+import org.apache.flink.streaming.api.datastream.KeyedStream;
+import org.apache.flink.streaming.api.datastream.SingleOutputStreamOperator;
+import org.apache.flink.streaming.api.environment.StreamExecutionEnvironment;
+import org.apache.flink.util.Collector;
+
+public class _7ReduceTransformation {
+    public static void main(String[] args) throws Exception {
+        StreamExecutionEnvironment env = StreamExecutionEnvironment.getExecutionEnvironment();
+
+        DataStreamSource<String> lines = env.socketTextStream("localhost",9999);
+
+        SingleOutputStreamOperator<Tuple2<String, Integer>> wordAndOne = lines.flatMap(new FlatMapFunction<String, Tuple2<String, Integer>>() {
+            @Override
+            public void flatMap(String text, Collector<Tuple2<String, Integer>> collector) throws Exception {
+                String[] words = text.toLowerCase().split(" ");
+                for (String word : words) {
+                    collector.collect(Tuple2.of(word, 1));
+                }
+            }
+        });
+
+        KeyedStream<Tuple2<String, Integer>, Tuple> keyed = wordAndOne.keyBy(0);
+
+        // 自己创建一个 reduce 函数来对分组后的结果操作
+        SingleOutputStreamOperator<Tuple2<String, Integer>> sumed = keyed.reduce(new ReduceFunction<Tuple2<String, Integer>>() {
+            @Override
+            public Tuple2<String, Integer> reduce(Tuple2<String, Integer> t0, Tuple2<String, Integer> t1) throws Exception {
+                // 次数获取 t0 和 t1 的第一个参数都是相同的,因为分组后第一个参数相同
+//                String key = t0.f0;
+//                int count1 = t0.f1;
+//                int count2 = t1.f1;
+//                int count = count1 + count2;
+//                return Tuple2.of(key,count);
+                t0.f1 = t0.f1 + t1.f1;
+                return t0;
+            }
+        });
+
+        sumed.print();
+
+        // 5 启动程序
+        env.execute("WordCunt");
+    }
+}
